ea61e019031782744781aaf3b85cd25ac64c29dd
mkeksz <mrkeksz@yandex.ru>
2021-09-25 16:10:13 +0300

    Remove callbackQuery.ts Update locales

--- src/util/callbackQuery.ts
+++
@@ -1,6 +0,0 @@
-import {BotContext} from '@/src/types/telegraf'
-
-export function getCallbackQueryData(ctx: BotContext): string {
-  if (!ctx.callbackQuery || !('data' in ctx.callbackQuery)) return ''
-  return ctx.callbackQuery.data
-}
