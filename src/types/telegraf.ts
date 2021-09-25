bfefd45101d0f534557c0ec49e40b7b0bb0d340a
mkeksz <mrkeksz@yandex.ru>
2021-09-25 19:33:23 +0300

    Improve types/telegraf.ts

--- src/types/telegraf.ts
+++ src/types/telegraf.ts
@@ -1,13 +1,13 @@
-import {BaseScene, SceneContext} from 'telegraf/typings/scenes'
 import {Update} from 'telegraf/typings/core/types/typegram'
+import {NarrowedContext, Scenes, Types} from 'telegraf'
 import * as http from 'http'
-import Storage from '@/src/Storage/Storage'
+import Storage from '../Storage/Storage'
 
 export type WebhookCallback = (req: (http.IncomingMessage & { body?: Update | undefined }), res: http.ServerResponse, next?: () => void) => Promise<void>
 
-export type Scene = BaseScene<SceneContext>
+export type Scene = Scenes.BaseScene<Scenes.SceneContext>
 
-export type ActionContext = BotContext & {match: RegExpExecArray}
-export interface BotContext extends SceneContext {
+export type ActionContext = NarrowedContext<BotContext & { match: RegExpExecArray }, Types.MountMap['callback_query']>
+export interface BotContext extends Scenes.SceneContext {
   storage: Storage
 }
